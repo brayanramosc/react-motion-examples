ab29d6ffcd3d75f7b5427727d02f3ea8c09b20bc
Brayan Ramos <brayanrcaicedo@gmail.com>
2023-12-08 15:33:20 -0500

    test variant property

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import './App.css';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
+import './App.css';
+
+const variants = {
+  odd: {
+    backgroundColor: '#222',
+    color: '#ccc'
+  },
+  even: {
+    backgroundColor: '#bbb',
+    color: '#000'
+  }
+}
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
       <motion.div
         className='box'
-        // initial={{ scale: 1 }}
-        // animate={{ scale: 2, rotate: 45 }}
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ["20%", "20%", "50%", "50%", "20%"]
-        }}
-        transition={{ duration: 1 }}
-      />
-
-      {/* <motion.h1
-        initial={{
-          color: '#ff0000'
-        }}
-        transition={{ duration: 5 }}
-        animate={{
-          y: 100,
-          color: '#000000'
+        variants={variants}
+        animate={counter % 2 === 0 ? 'even' : 'odd'}
+        drag='x'
+        dragDirectionLock
+        dragConstraints={{
+          top: -50,
+          left: -50,
+          right: 50,
+          bottom: 50
         }}
       >
-        Hallo Welt!
-      </motion.h1> */}
+        {counter}
+      </motion.div>
+      <button onClick={() => setCounter(counter => counter + 1)}>
+        Add
+      </button>
     </div>
   )
 }
